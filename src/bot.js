428432cfa151e1bd9b77fd77adbd7cd598c59541
Александр Рась <dolrest@student.21-school.ru>
2024-03-29 20:18:41 +0700

    Time Calendar

--- src/bot.js
+++ src/bot.js
@@ -1,7 +1,7 @@
 // Библиотеки
 const { Telegraf, session, Stage, Scenes, Markup } = require('telegraf');
 require('dotenv').config()
-const bot = new Telegraf(process.env.BOT_TOKEN, { polling: true });
+const bot = new Telegraf(process.env.BOT_TOKEN);
 
 // База данных и таблицы
 const sequelize = require('./database/database'); // Настройки модели БД
@@ -40,18 +40,7 @@ bot.command('signup', async (ctx) => {
     ctx.scene.enter(`signupScene`)
 })
 
-bot.command('calendar', async (ctx) => {
-    calendar.startNavCalendar(ctx.message);
-})
 
-bot.on("callback_query", (ctx) => {
-    if (ctx.callbackQuery.message.message_id == calendar.chats.get(ctx.callbackQuery.message.chat.id)) {
-        res = calendar.clickButtonCalendar(ctx.callbackQuery);
-        if (res !== -1) {
-            bot.telegram.sendMessage(ctx.callbackQuery.message.chat.id, "You selected: " + res);
-        }
-    }
-});
 
 
 
