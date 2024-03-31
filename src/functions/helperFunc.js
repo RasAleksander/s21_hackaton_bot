2b83e12b67476616651ca5ca9d5366c4de65045c
Eboni Crab <ebonicra@in-h4.nsk.21-school.ru>
2024-03-31 10:51:47 +0700

    чистка helper and sendemails

--- src/functions/helperFunc.js
+++ src/functions/helperFunc.js
@@ -1,13 +1,15 @@
 const { Telegraf, Markup, Input } = require('telegraf')
-const { Sequelize } = require('sequelize');
+const Sequelize = require('sequelize');
 const moment = require('moment');
 const axios = require('axios');
-require('dotenv').config()
-const sequelize = require('../database/database');
+
 const Visit = require('../database/VisitLog.js');
-// const Admin = require('../database/AdminModel'); // Модель списка админов
-const Profile = require('../database/ProfilePeer'); // Модель списка админов
-const MeetingRoom = require('../database/MeetingRoom'); // Модель списка админов
+const Admin = require('../database/ProfileAdmin');
+const Profile = require('../database/ProfilePeer');
+const MeetingRoom = require('../database/MeetingRoom');
+const TokenPeer = require('../database/TokenPeer');
+
+require('dotenv').config()
 
 class helperFunction {
     static async doesUserNickname(id) {
@@ -15,7 +17,6 @@ class helperFunction {
         if (user) {
             return user.nickname;
         } else { return user }
-
     }
 
     static async checkQR(ctx, room) {
@@ -39,41 +40,65 @@ class helperFunction {
         return user;
     }
 
+    static async doesAdminExist(id) {
+        const user = await Profile.findOne({ where: { id_tg: id } });
+        if (!user) {
+            return false;
+        }
+        const isAdmin = await Admin.findOne({ where: { peer_id: user.id } });
+        return isAdmin;
+    }
+
     static async sendMessage(id_tg, message) {
         try {
-            const token = process.env.BOT_TOKEN; // Замените на ваш токен бота
+            const token = process.env.BOT_TOKEN;
             const url = `https://api.telegram.org/bot${token}/sendMessage`;
             const payload = {
                 chat_id: id_tg,
                 text: message
             };
-            const response = await axios.post(url, payload);
-            console.log('Message sent:', response.data);
+            const response = await axios.post(url, payload)
         } catch (error) {
             console.error('Error sending message:', error.message);
         }
-
     }
 
     static async runScript() {
-        // Найти текущее время и время, которое наступит через 59 минут
         const currentTime = moment();
-        const endTimeThreshold = moment().add(15, 'minutes');
+        const endTimeThreshold = moment().add(16, 'minutes');
+        const startTimeThreshold = moment().subtract(16, 'minutes'); // Время, наступающее через 15 минут назад
 
         // Найти все записи, у которых время окончания находится в промежутке между текущим временем и endTimeThreshold
         const bookings = await Visit.findAll({
             where: {
                 [Sequelize.Op.and]: [
                     {
-                        end_time: {
-                            [Sequelize.Op.lte]: endTimeThreshold.toDate() // Найти записи, где end_time меньше или равно времени, через 59 минут
-                        }
-
+                        [Sequelize.Op.or]: [
+                            {
+                                end_time: {
+                                    [Sequelize.Op.lte]: endTimeThreshold.toDate()
+                                }
+                            },
+                            {
+                                start_time: {
+                                    [Sequelize.Op.lte]: startTimeThreshold.toDate()
+                                }
+                            }
+                        ]
                     },
                     {
-                        end_time: {
-                            [Sequelize.Op.gte]: currentTime.toDate() // И одновременно больше или равно текущему времени
-                        }
+                        [Sequelize.Op.or]: [
+                            {
+                                end_time: {
+                                    [Sequelize.Op.gte]: currentTime.toDate()
+                                }
+                            },
+                            {
+                                start_time: {
+                                    [Sequelize.Op.gte]: currentTime.toDate()
+                                }
+                            }
+                        ]
                     }
                 ]
             }
@@ -82,20 +107,87 @@ class helperFunction {
         // Отправить сообщение всем пользователям из таблицы Visit
         for (const booking of bookings) {
             const { peer_id } = booking;
-            const profile = await Profile.findOne({ where: { id: peer_id } }); // Найти профиль пользователя по его идентификатору
-            console.log('пирайди' + peer_id)
+             // Найти профиль пользователя по его идентификатору
+            const profile = await Profile.findOne({ where: { id: peer_id } });
             const { id_tg } = profile;
-            console.log('\nТгайди' + id_tg)
             if (profile) {
-                // const { id_tg } = profile;
-                console.log('Зашли в рассылку')
                 const timeDiffMinutes = moment(booking.end_time).diff(currentTime, 'minutes');
-                const message = `Ваше бронирование заканчивается через ${timeDiffMinutes} минут`;
+                const message = `Ваше бронирование начинается/заканчивается через ${timeDiffMinutes} минут`;
                 await helperFunction.sendMessage(id_tg, message);
             }
         }
     }
 
+    static async setStartTime(date) {
+        let now = moment();
+        if (moment().format('YYYY-MM-DD') == date)
+            now.minutes(Math.ceil(now.minutes() / 15) * 15)
+        else {
+            now.minutes('00')
+            now.hours('00')
+        }
+        now = now.format('HH:mm')
+        return now;
+    }
+
+    static async addToLimitByVisitId(visitId) {
+        try {
+            // Находим запись в таблице VisitLog по id
+            const visit = await Visit.findByPk(visitId);
+            if (!visit) {
+                throw new Error('Запись с таким id не найдена');
+            }
+            const start = visit.start_time.getTime();
+            const end = visit.end_time.getTime();
+            const differenceInMinutes = Math.ceil((end - start) / (1000 * 60));
+
+            const user = await Profile.findByPk(visit.peer_id);
+            if (!user) {
+                throw new Error('Пользователь с таким peer_id не найден');
+            }
+
+            user.limit += differenceInMinutes;
+            await user.save();
+
+            return `Лимит пользователя ${user.nickname} успешно обновлен на ${differenceInMinutes} минут`;
+        } catch (error) {
+            return `Произошла ошибка: ${error.message}`;
+        }
+    }
+
+    static async updateStatusAndAddToLimit() {
+        try {
+            // Находим все записи в таблице VisitLog, у которых разница между end_time и текущим временем равна 0 в минутах
+            const currentDateTime = moment();
+            const expiredVisits = await Visit.findAll({
+                where: {
+                    end_time: {
+                         // Находим записи, у которых end_time меньше или равно текущему времени
+                        [Sequelize.Op.lte]: currentDateTime.toDate()
+                    }
+                }
+            });
+            // Обновляем статус для найденных записей и вызываем функцию addToLimitByVisitId
+            for (const visit of expiredVisits) {
+                visit.status = 2; // Устанавливаем статус 'проведено' (или любой другой, который вам нужен)
+                await visit.save();
+                await helperFunction.addToLimitByVisitId(visit.id);
+            }
+            return `Успешно обновлено ${expiredVisits.length} записей и добавлено к лимиту`;
+        } catch (error) {
+            return `Произошла ошибка: ${error.message}`;
+        }
+    }
+
+    static async checkToken(nickname, token) {
+        try {
+          const user = await TokenPeer.findOne({ where: { nickname, token } });
+          return !!user; // Возвращаем true, если пользователь найден, иначе false
+        } catch (error) {
+          console.error('Ошибка при проверке токена:', error);
+          throw new Error('Ошибка при проверке токена');
+        }
+      }
 }
 
 module.exports = helperFunction;
